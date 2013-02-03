2e344bc563ec459c2b561b63c0c36a7e3fec634d
Brian Flad <bflad417@gmail.com>
2013-02-03 11:54:21 -0500

    Metadata fixes

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,14 @@
+name             "cfsolr"
 maintainer       "The Wharton School - The University of Pennsylvania"
-maintainer_email "nmische@wharton.upenn.edu"
+maintainer_email "chef-admins@wharton.upenn.edu"
 license          "Apache 2.0"
-description      "Installs Apache SOLR Standalone for ColdFusion 9.0.1"
+description      "Installs Apache Solr Standalone for ColdFusion"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe "cfsolr", "Installs Apache Solr Standalone for ColdFusion"
 
 %w{ ubuntu }.each do |os|
   supports os
 end
-
-recipe "cfsolr", "Installs Apache SOLR Standalone for ColdFusion 9.0.1"
 
